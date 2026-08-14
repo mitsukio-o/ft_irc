dfc8de9c7e0985bbc763546be08986ae4f0aac22
yuknakas <yukinakashiba@gmail.com>
2026-08-14 17:47:53 +0900

    add: channel full draft1 channel の関数をひとまず実装しました
    【初期との変更点】
    Channel::broadcast() 内で Client::push() を利用するために、std::set<const Client*> を std::set<Client*> に変更しました。
    Client::push() 関数が const 関数でなかったため、この変更が必要でした。
    結果として、他の std::set も const Client* から Client* に変更しました。
    安全面では少し心配になるので、後ほど精査します。

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -13,21 +13,21 @@ public:
 	explicit Channel(const std::string& name);
 	~Channel();
 
-	const std::string&				getName() const;
-	const std::string&				getTopic() const;
-	const std::string&				getKey() const;
-	size_t							getLimit() const;
-	bool							isInviteOnly() const;
-	bool							isTopicLocked() const;
-	std::string						getModes() const;
-	std::string						getNames() const;
-	const std::set<const Client*>&	getMembers() const;
+	const std::string&			getName() const;
+	const std::string&			getTopic() const;
+	const std::string&			getKey() const;
+	size_t						getLimit() const;
+	bool						isInviteOnly() const;
+	bool						isTopicLocked() const;
+	std::string					getModes() const;
+	std::string					getNames() const;
+	const std::set<Client*>&	getMembers() const;
 
 	bool	isEmpty() const;
-	bool	has(const Client& client) const;
-	bool	isOperator(const Client& client) const;
-	bool	isInvited(const Client& client) const;
-	const Client*	find(const std::string& nick) const;
+	bool	has(Client& client) const;
+	bool	isOperator(Client& client) const;
+	bool	isInvited(Client& client) const;
+	Client*	find(const std::string& nick) const;
 
 	void	setTopic(const std::string& topic);
 	void	setKey(const std::string& key);
@@ -36,21 +36,21 @@ public:
 	void	setTopicLocked(bool on);
 
 	void	join(Client* client, bool asOperator);
-	void	leave(const Client& client);
-	void	invite(const Client& client);
-	void	grant(const Client& client, bool on);
-	void	broadcast(const std::string& message, const Client* except = 0) const;
+	void	leave(Client& client);
+	void	invite(Client& client);
+	void	grant(Client& client, bool on);
+	void	broadcast(const std::string& message, Client* except = 0);
 
 private:
-	std::string				_name;
-	std::string				_topic;
-	std::string				_key;
-	size_t					_limit;
-	bool					_inviteOnly;
-	bool					_topicLocked;
-	std::set<const Client*>	_members;
-	std::set<const Client*>	_operators;
-	std::set<const Client*>	_invited;
+	std::string			_name;
+	std::string			_topic;
+	std::string			_key;
+	size_t				_limit;
+	bool				_inviteOnly;
+	bool				_topicLocked;
+	std::set<Client*>	_members;
+	std::set<Client*>	_operators;
+	std::set<Client*>	_invited;
 
 	Channel(const Channel& other);
 	Channel&	operator=(const Channel& other);
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -17,7 +17,7 @@ size_t							Channel::getLimit() const		{ return (_limit); }
 bool							Channel::isInviteOnly() const	{ return (_inviteOnly); }
 bool							Channel::isTopicLocked() const	{ return (_topicLocked); }
 bool							Channel::isEmpty() const		{ return (_members.empty()); }
-const std::set<const Client*>&	Channel::getMembers() const		{ return (_members); }
+const std::set<Client*>&		Channel::getMembers() const		{ return (_members); }
 
 void	Channel::setTopic(const std::string& topic)	{ _topic = topic; }
 void	Channel::setKey(const std::string& key)		{ _key = key; }
@@ -25,19 +25,42 @@ void	Channel::setLimit(size_t limit)				{ _limit = limit; }
 void	Channel::setInviteOnly(bool on)				{ _inviteOnly = on; }
 void	Channel::setTopicLocked(bool on)			{ _topicLocked = on; }
 
+// returns itky (invite, topic lock, password, limit)
 std::string	Channel::getModes() const
 {
-	return ("+");
+	std::string modes;
+
+	if (_inviteOnly)
+		modes += "i";
+	if (_topicLocked)
+		modes += "t";
+	if (!_key.empty())
+		modes += "k";
+	if (_limit != 0)
+		modes += "l";
+	return (modes);
 }
 
+// returns std::string of nicknames
 std::string	Channel::getNames() const
 {
-	return ("");
+	std::string	names;
+	std::set<Client*>::iterator	it;
+
+	for (it = _members.begin(); it != _members.end(); ++it)
+	{
+		if (it != _members.begin())
+			names += " ";
+		if (isOperator(**it))
+			names += "@";
+		names += (*it)->getNick();
+	}
+	return (names);
 }
 
 
 // finds if client is a member of the channel
-bool	Channel::has(const Client& client) const
+bool	Channel::has(Client& client) const
 {
 	if (_members.find(&client) == _members.end())
 		return (false);
@@ -45,7 +68,7 @@ bool	Channel::has(const Client& client) const
 }
 
 // finds if client is an operator of the channel
-bool	Channel::isOperator(const Client& client) const
+bool	Channel::isOperator(Client& client) const
 {
 	if (_operators.find(&client) == _members.end())
 		return (false);
@@ -53,16 +76,16 @@ bool	Channel::isOperator(const Client& client) const
 }
 
 // finds if client is invited to the channel
-bool	Channel::isInvited(const Client& client) const
+bool	Channel::isInvited(Client& client) const
 {
 	if (_invited.find(&client) == _members.end())
 		return (false);
 	return (true);
 }
 
-const Client*	Channel::find(const std::string& nick) const
+Client*	Channel::find(const std::string& nick) const
 {
-	std::set<const Client*>::iterator	it;
+	std::set<Client*>::iterator	it;
 
 	for (it = _members.begin(); it != _members.end(); ++it)
 	{
@@ -74,20 +97,38 @@ const Client*	Channel::find(const std::string& nick) const
 
 void	Channel::join(Client* client, bool asOperator)
 {
-	(void)client;
-	(void)asOperator;
+	_members.insert(client);
+	_invited.erase(client);
+	if (asOperator)
+		_operators.insert(client);
 }
 
-void	Channel::leave(const Client& client)
+void	Channel::leave(Client& client)
 {
-	(void)client;
+	_members.erase(&client);
+	_operators.erase(&client);
 }
 
-void	Channel::invite(const Client& client)			{ (void)client; }
-void	Channel::grant(const Client& client, bool on)	{ (void)client; (void)on; }
-
-void	Channel::broadcast(const std::string& message, const Client* except) const
+void	Channel::invite(Client& client)
 {
-	(void)message;
-	(void)except;
+	_invited.insert(&client);
+}
+
+void	Channel::grant(Client& client, bool on)
+{
+	if (on)
+		_operators.insert(&client);
+	else
+		_operators.erase(&client);
+}
+
+void	Channel::broadcast(const std::string& message, Client* except)
+{
+	std::set<Client*>::iterator	it;
+
+	for (it = _members.begin(); it != _members.end(); ++it)
+	{
+		if (*it != except)
+			(*it)->push(message);
+	}
 }
